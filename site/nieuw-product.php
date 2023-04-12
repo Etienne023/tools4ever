24ccc01861b41f295c0ddf5034401aee875f469c
Etienne van Bergeijk <etiennevanbergeijk@gmail.com>
2023-04-12 14:51:34 +0200

    css

--- site/nieuw-product.php
+++ site/nieuw-product.php
@@ -1,19 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>nieuw product invoeren</title>
 </head>
+
 <body>
     <form action="" method="post">
-        
-        <label for="">naam product</label>
+        <div class="form-groep">
+            <label for="NaamProduct">naam product</label>
+            <input type="text" name="" id="NaamProduct">
+        </div>
+        <div class="form-groep">
+            <label for="catogorieProduct">catogorie product</label>
+            <input type="text" name="" id="catogorieProduct">
+        </div>
+        <div class="form-groep">
+            <label for="prijsProduct">prijs product</label>
+            <input type="text" name="" id="prijsProduct">
 
-        <input type="text" name="" id="">
+        </div>
+        <div class="form-groep">
+            <label for="merkProduct">merk product</label>
+            <input type="text" name="" id="merkProduct">
+        </div>
+
+
+
+
+
+
+
 
         <button type="submit">verzend</button>
     </form>
 </body>
+
 </html>
